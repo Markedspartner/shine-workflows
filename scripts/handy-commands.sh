7c5fb85bad3fab303d40f93b92b0049099f813c3
kenleren <ken@markedspartner.no>
2025-03-07 12:57:47 +0100

    tralala.. Ditta funket jo

--- scripts/handy-commands.sh
+++
@@ -1,5 +0,0 @@
-docker build -t shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179359433:latest .
-
-docker tag image-workflow-1741179359433:latest shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179359433:latest
-
-docker push shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179359433:latest
