8c5e15ef4d4f48aa51cb72b3c39dba4c09dda55d
kenleren <ken@markedspartner.no>
2025-03-07 13:13:18 +0100

    fix

--- scripts/handy-commands.sh
+++ scripts/handy-commands.sh
@@ -2,6 +2,4 @@ docker build -t shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179
 
 docker tag image-workflow-1741179359433:latest shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179359433:latest
 
-docker login -u ken-leren -p  shinekubernetesclusterregistry.azurecr.io
-
 docker push shinekubernetesclusterregistry.azurecr.io/image-workflow-1741179359433:latest
